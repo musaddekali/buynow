cf99c40b05f8d500cf39a0cc939fd53f0bacc243
musaddekali <musaddekali.dev@gmail.com>
2022-06-14 12:02:22 +0600

    api .env.local keys called

--- src/context/firebase.js
+++ src/context/firebase.js
@@ -2,16 +2,27 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCBODWPaNdG2vwFMf_TcRVnOGBPQUrPjSk",
-  authDomain: "buynow-1.firebaseapp.com",
-  projectId: "buynow-1",
-  storageBucket: "buynow-1.appspot.com",
-  messagingSenderId: "35338344803",
-  appId: "1:35338344803:web:8ffbcced79b361996c5b56"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+
+
+// firebase keys.
+// REACT_APP_API_KEY = AIzaSyCBODWPaNdG2vwFMf_TcRVnOGBPQUrPjSk
+// REACT_APP_AUTH_DOMAIN  = buynow-1.firebaseapp.com
+// REACT_APP_PROJECT_ID = buynow-1
+// REACT_APP_STORAGE_BUCKET = buynow-1.appspot.com
+// REACT_APP_MESSAGEING_SENDER_ID = 35338344803
+// REACT_APP_APP_ID = 1:35338344803:web:8ffbcced79b361996c5b56
+
 
 
